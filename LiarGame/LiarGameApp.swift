bc59e9fdc96f21161790638db100bf70230c62f4
agilestarskim <agilestarskim@naver.com>
2022-06-30 18:07:32 +0900

    Initial Commit

---
+++ LiarGame/LiarGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiarGameApp.swift
+//  LiarGame
+//
+//  Created by 김민성 on 2022/06/30.
+//
+
+import SwiftUI
+
+@main
+struct LiarGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
